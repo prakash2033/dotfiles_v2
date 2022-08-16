83b7b57e269b9bbd322ddec916be6acbbf0ed54e
Prakash Vishwakarma <pvishwa1@volvocars.com>
2022-08-16 10:55:53 +0200

    Clock calender widget on top panel

--- normal_user/awesome/.config/awesome/layout/top-panel.lua
+++ normal_user/awesome/.config/awesome/layout/top-panel.lua
@@ -11,6 +11,23 @@ local dpi = require('beautiful').xresources.apply_dpi
 
 local icons = require('theme.icons')
 
+-- Clock / Calendar 24h format
+local textclock = wibox.widget.textclock('<span font="Roboto Mono bold 9">%d.%m.%Y\n     %H:%M</span>')
+
+-- Clock / Calendar 12AM/PM fornat
+-- local textclock = wibox.widget.textclock('<span font="Roboto Mono bold 9">%d.%m.%Y\n  %I:%M %p</span>\n<span font="Roboto Mono bold 9">%p</span>')
+-- textclock.forced_height = 56
+
+-- Add a calendar (credits to kylekewley for the original code)
+local month_calendar = awful.widget.calendar_popup.month({
+  screen = s,
+  start_sunday = false,
+  week_numbers = true
+})
+month_calendar:attach(textclock)
+
+local clock_widget = wibox.container.margin(textclock, dpi(13), dpi(13), dpi(8), dpi(8))
+
 local add_button = mat_icon_button(mat_icon(icons.plus, dpi(24)))
 add_button:buttons(
   gears.table.join(
@@ -110,6 +127,8 @@ local TopPanel = function(s, offset)
     nil,
     {
       layout = wibox.layout.fixed.horizontal,
+      -- Clock
+      clock_widget,
       -- Layout box
       LayoutBox(s)
     }
