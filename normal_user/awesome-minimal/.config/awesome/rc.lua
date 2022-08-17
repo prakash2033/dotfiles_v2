a733527fef585e3764ac2fb309391291ec300a9a
Prakash Vishwakarma <pvishwa1@volvocars.com>
2022-08-17 19:31:33 +0200

    clean up

--- normal_user/awesome-minimal/.config/awesome/rc.lua
+++ normal_user/awesome-minimal/.config/awesome/rc.lua
@@ -71,7 +71,6 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-
     awful.layout.suit.tile,
     awful.layout.suit.max,
     awful.layout.suit.tile,
