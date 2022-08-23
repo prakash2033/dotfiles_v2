d3057d3ef443c502e1b3c9bd016e1eec92d2e6b0
Prakash Vishwakarma <prakash2033@outlook.com>
2022-08-23 15:06:10 +0200

    removed some bindings

--- normal_user/awesome-minimal/.config/awesome/rc.lua
+++ normal_user/awesome-minimal/.config/awesome/rc.lua
@@ -371,16 +371,6 @@ for i = 1, 9 do
                         end
                   end,
                   {description = "view tag #"..i, group = "tag"}),
-        -- Toggle tag display.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = awful.screen.focused()
-                      local tag = screen.tags[i]
-                      if tag then
-                         awful.tag.viewtoggle(tag)
-                      end
-                  end,
-                  {description = "toggle tag #" .. i, group = "tag"}),
         -- Move client to tag.
         awful.key({ modkey, "Shift" }, "#" .. i + 9,
                   function ()
@@ -388,21 +378,11 @@ for i = 1, 9 do
                           local tag = client.focus.screen.tags[i]
                           if tag then
                               client.focus:move_to_tag(tag)
+                              tag:view_only()
                           end
                      end
                   end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
-        -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:toggle_tag(tag)
-                          end
-                      end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
+                  {description = "move focused client to tag #"..i, group = "tag"})
     )
 end
 
@@ -569,7 +549,7 @@ awful.spawn.with_shell("pkill pnmixer")
 awful.spawn.with_shell("pkill variety")
 awful.spawn.with_shell("picom -b --experimental-backends")
 awful.spawn.with_shell("xfce4-power-manager")
-awful.spawn.with_shell("autorandr --load officedock")
+awful.spawn.with_shell("autorandr --load homedock")
 awful.spawn.with_shell("setxkbmap -layout 'se,us'")
 awful.spawn.with_shell("dropbox")
 awful.spawn.with_shell("flameshot")
